49606854f0804c1747909c9591318307c5164abd
RAHUL1231998 <rapr16cs@cmrit.ac.in>
2018-02-20 19:48:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/urlone', function(req,res){
+   res.send('URL 1 article sent'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
