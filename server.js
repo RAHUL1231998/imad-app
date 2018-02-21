14e9f867a0371921b704e438f65844a7a85c2b8a
RAHUL1231998 <rapr16cs@cmrit.ac.in>
2018-02-21 20:21:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function(req,res){
      var articleName = req.params.articlename;
-     res.sendFile(createTemplate(articles[articleName]));
+     res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
