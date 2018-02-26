a0cf22d1b8ca63bb3a3ecaad74dbb042f5c844f9
RAHUL1231998 <rapr16cs@cmrit.ac.in>
2018-02-26 22:37:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,128 +2,99 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var app = express();
-app.use(morgan('combined'));
-
-var articles = {
-'article-one' : { 
-title: 'Article One | Deepak Kumar',
-heading: 'Article One',
-date: 'feb 21,2018',
-content: 
-<p>
-This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-</p>
-<p>
-This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-</p>
-<p>
-This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-</p>
-
-},
-'article-two' : {
-    title: 'Article Two | Deepak Kumar',
-    heading: 'Article Two',
-    date: 'feb 22,2018',
-    content: `
-    <p>
-        This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
-    </p>`
-},
-'article-three' : {
-    title: 'Article Three | Deepak Kumar',
-    heading: 'Article Three',
-    date: 'feb 23,2018',
-    content: `
-    <p>
-        This is the content for my third article.
-    </p>`
-},
+var articles=
+{ 
+            'article-one':{
+                        title:'Article 1 page',
+                        heading:'Welcome',
+                        date:'20t feb 2018',
+                        content:`<p>
+                                                this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article
+                                            </p>
+                                            <p>
+                                                this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article
+                                            </p>
+                                           
+                                            <p>
+                                                this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article.this is content of 1st article
+                                            </p>`
+            },
+            'article-two':{
+                        title:'Article 2 page',
+                        heading:'Welcome',
+                        date:'24t feb 2018',
+                        content:`<p>
+                                   this is content of 2nd artice                
+                                </p>`
+            },
+            'article-three':{
+                        title:'Article 3 page',
+                        heading:'Welcome',
+                        date:'12th feb 2018',
+                        content:`<p>
+                                    tis is content if 3rd article           
+                                </p>`
+            }
 };
-
-function createTemplate(data){
+function createTemplate(data)
+{
 var title = data.title;
 var date = data.date;
 var heading = data.heading;
 var content = data.content;
-
-var htmlTemplate=`
-<html>
+var htmltemplate=`<html>
     <head>
         <title>
-            Article One | Deepak Kumar
+            ${title}
         </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" width="device-widt,initil-scale=1"/>
         <link href="/ui/style.css" rel="stylesheet" />
     </head>
-<body>
-    <div class="container">
-    <div>
-        <a href="/">Home</a>
+    <body>
+        <div class="maincnt">
+            <div>
+                <a href="/">HOME</a>
+            </div>
+            <hr/>
+            <h3>${heading}</h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                       ${content}
+            </div>
     </div>
-    <hr/>
-    <h3>
-      ${heading}
-    </h3>
-    <div>
-      ${date}
-    </div>
-    <div>
-      ${content}
-    </div>
-    </div>
-</body>    
-</html>
-`;
-return htmlTemplate;
+    </body>
+</html>`;
+return htmltemplate; 
 }
+var app = express();
+app.use(morgan('combined'));
 
-app.get('/' , function (req , res) {
-res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-counter = counter + 1;
-res.send(counter.toString());
-});
-
-var names = [];
-app.get('/submit-name', function(req, res) {
-//get the name from request
-var name = req.query.name;
-
-names.push(name);
-//JSON: Javascript Object Notation
-res.send(JSON.stringify(names));
-});
-
-app.get('/:articleName', function (req, res) {
-//article == article-one
-//articles[articleName] == {} content object for article one
-var articleName = req.params.articleName;
-res.send(createTemplate(articles[articleName]));
+app.get('/:articleName', function(req,res){
+     var articleName = req.params.articlename;
+     res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
 app.listen(port, function () {
-console.log(IMAD course app listening on port ${port}!);
+  console.log(`IMAD course app listening on port ${port}!`);
 });
 
 
