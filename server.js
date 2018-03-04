3f36940053bb734e914727b64d8b06c51d7324dd
RAHUL1231998 <rapr16cs@cmrit.ac.in>
2018-03-04 15:06:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/test-db', function(req,res){
        if(err){
            res.status(500).send(err.toString());
        }
-       esle{
+       else{
            res.send(JSON.stringify(result));
        }
    }); 
@@ -127,7 +127,20 @@ app.get('/articles/:articleName', function (req, res) {
     //article == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
-    pool.query("SELECT*FROM article WHERE tilte =");
+    pool.query("SELECT*FROM article WHERE tilte = "+ req.params.articleName, function(err,result){
+        if(err){
+           res.status(500).send(err.toString());
+       }
+       else{
+           if(result.rows.length === 0){
+            res.status(404).send('Article not found');
+           }
+           else{
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
+    });
     res.send(createTemplate(articleData));
 });
  
